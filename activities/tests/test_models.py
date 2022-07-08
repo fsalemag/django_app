4018599f3e42ed63a5292198e665c686fb6e6299
Francisco Salema <francisco.salema@trustyou.net>
2022-07-08 21:41:08 +0200

    Updated to pytest 100%

--- activities/tests/test_models.py
+++ activities/tests/test_models.py
@@ -1,90 +1,44 @@
-from datetime import timedelta
-
-from django.contrib.auth.models import AnonymousUser
-from django.test import RequestFactory, TestCase
-from django.utils import timezone
-
-from users.models import MyUser
-from utils.test_utils import create_activities_and_categories, create_user_and_profile
 from ..models import Category, Activity, ActivityVote
 
 
-class ActivityModelTest(TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-
-        activities = {
-            "football": {
-                "count": 10,
-                "date": timezone.now() - timedelta(days=5),
-                "max_participants": 1,
-            },
-            "tennis": {
-                "count": 7,
-                "date": timezone.now() + timedelta(days=4),
-            },
-            "padel": {
-                "count": 3,
-                "date": timezone.now() - timedelta(days=5),
-            }
-        }
-
-        user, _ = create_user_and_profile("dummy@dummy.com")
-        create_activities_and_categories(activities)
-
-    @classmethod
-    def tearDownClass(cls):
-        super().tearDownClass()
-
-    def setUp(self) -> None:
-        self.anonymous_user = AnonymousUser()
-        self.my_user = MyUser.objects.get(email="dummy@dummy.com")
-        self.factory = RequestFactory()
-
-    def tearDown(self) -> None:
-        pass
+class TestActivityModel:
 
     def test_category_str(self):
         category = Category.objects.get(name="football")
-        self.assertEqual(category.__str__(), "football")
+        assert category.__str__() == "football"
 
-    def test_activity_vote_str(self):
-        activity_vote = ActivityVote.objects.create(voter=self.my_user, score=3)
-        self.assertEqual(activity_vote.__str__(), "dummy@dummy.com: 3 (1)")
+    def test_activity_vote_str(self, my_user):
+        activity_vote = ActivityVote.objects.create(voter=my_user, score=3)
+        assert activity_vote.__str__() == "dummy@dummy.com: 3 (1)"
 
     def test_activity_str(self):
         activity = Activity.objects.all()[0]
-        self.assertEqual(activity.__str__(), "Dummy Activity")
+        assert activity.__str__() == "Dummy Activity"
 
     def test_activity_is_past(self):
         activity = Activity.objects.filter(category__name="football")[0]
-        self.assertTrue(activity.is_past)
+        assert activity.is_past
 
         activity = Activity.objects.filter(category__name="tennis")[0]
-        self.assertFalse(activity.is_past)
+        assert not activity.is_past
 
-    def test_activity_is_full(self):
+    def test_activity_is_full(self, my_user):
         activity = Activity.objects.filter(max_n_participants=1)[0]
-        self.assertFalse(activity.is_full)
+        assert not activity.is_full
 
-        activity.participants.add(self.my_user)
-        self.assertTrue(activity.is_full)
+        activity.participants.add(my_user)
+        assert activity.is_full
 
-    def test_activity_score(self):
+    def test_activity_score(self, my_user):
         activity = Activity.objects.filter(category__name="padel")[0]
-        self.assertIsNone(activity.score)
+        assert activity.score is None
 
         for i in range(1, 5):
-            vote = ActivityVote.objects.create(voter=self.my_user, score=i)
+            vote = ActivityVote.objects.create(voter=my_user, score=i)
             activity.votes.add(vote)
 
-        self.assertEqual(activity.score, 2.5)
+        assert activity.score == 2.5
 
     def test_activity_absolute_url(self):
         activity = Activity.objects.get(pk=1)
-        self.assertEqual(
-            activity.get_absolute_url(),
-            '/activities/category/football/1'
-        )
+        assert activity.get_absolute_url() == '/activities/category/football/1'
